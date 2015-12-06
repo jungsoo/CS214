12bb157f3d77956aef19f87dc9f85c4ed5124140
Jungsoo Park <jungsoopark96@gmail.com>
2015-12-06 01:22:37 -0500

    fuck this shit i'm done for the night
    Began working on the server-side command handling.

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@ CFLAGS=-Wall -g -pthread
 
 all: server client
 
-server: server.c util.h
+server: server.c util.h tokenizer.h
 	$(CC) $(CFLAGS) -o server server.c
 
-client: client.c util.h
+client: client.c util.h tokenizer.h
 	$(CC) $(CFLAGS) -o client client.c
 
 clean:
--- server.c
+++ server.c
@@ -11,11 +11,11 @@
 #include <sys/mman.h>
 #include <netinet/in.h>
 #include "util.h"
+#include "tokenizer.h"
 
 #define FILEPATH        "/tmp/bankinfo.bin"
 #define MAX_ACCOUNT     (20)
 #define FILESIZE        (MAX_ACCOUNT * sizeof(account_t))
-
 
 typedef struct account {
     pthread_mutex_t mutex;
@@ -24,15 +24,181 @@ typedef struct account {
     int is_active;
 } account_t;
 
+/*
+ * Global Variables
+ */
 int bank_fd;
 account_t *bank;
 
+pthread_mutex_t accounts_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void open_account(char *accountname) {
+    for (int i = 0; i < MAX_ACCOUNT; i++) { 
+        if (streq(bank[i].name, accountname)) {
+            return -2; // Account name already exists.
+        }
+        if (bank[i].name == NULL) {
+            bank[i].name = accountname;
+            bank[i].balance = 0;
+            bank[i].is_active = 0;
+
+            pthread_mutexattr_t attr;
+            pthread_mutexattr_init(&attr);
+            pthread_mutexattr_settype(%attr, PTHREAD_MUTEX_RECURSIVE);
+            if (pthread_mutex_init(&bank[i].mutex, &attr)) {
+                error("ERROR: Unable to initialize account mutex.");
+            }
+
+            return 1; // Account successfully opened.
+        }
+    }
+    return -1; // Bank is at capacity.
+}
+
 void client_service(int sock) {
     char request[2048];
+    int in_customer_session = 0; // Each client starts off out of customer session.
+    Tokenizer tokenizer;
+    int result;
+    char *cmd;
+    char *accountname;
 
     memset(&request, '0', sizeof(request));
+
     while(read(sock, request, sizeof(request)) > 0) {
         printf("(%d): %s\n", getpid(), request);
+
+        tokenizer = createTokenizer(request);
+
+        cmd = getNextToken(tokenizer);
+		if (streq(cmd, "open")) {
+			if (!(accountname = getNextToken(tokenizer)) || getNextToken(tokenizer)) {
+				write(sock, request, sprintf(request, "BANK: Invalid syntax.\n") + 1);
+			}
+            pthread_mutex_lock(&accounts_mutex);
+            result = open_account(accountname);
+            if (result == -1) {
+				write(sock, request, sprintf(request, "BANK: Twenty accounts exist already; cannot create another.\n") + 1);
+            } else if (result == -2) {
+				write(sock, request, sprintf(request, "BANK: Cannot create account %s - account name exists.\n", accountname) + 1);
+            } else {
+                write(sock, request, sprintf(request, "BANK: Successfully added account %s.\n", acc->name) + 1);
+            }
+            pthread_mutex_unlock(&accounts_mutex);
+		} else if (streq(cmd, "start")) {
+			if (!(accountname = getNextToken(tokenizer)) || getNextToken(tokenizer)) {
+				write(sock, request, sprintf(request, "BANK: Invalid syntax.\n") + 1);
+				continue;
+			}
+            // Iterate through bank to find the account.
+            int account_index = 0;
+            for (; account_index <= MAX_ACCOUNT; account_index++) {
+                if (streq(accountname, bank[account_index].name)) {
+                    break;
+                }
+            }
+
+            if (in_customer_session) {
+				write(sock, request, sprintf(request, "BANK: Already serving account %s.\n", accountname) + 1);
+			} else if (account_index >= MAX_ACCOUNT) {
+				write(sock, request, sprintf(request, "BANK: Account %s does not exist.\n", accountname) + 1);
+            } else if (bank[account_index].is_active) {
+				write(sock, request, sprintf(request, "BANK: Account %s is already in use.\n", accountname) + 1);
+			}
+            // TODO: ugh
+            
+			while(pthread_mutex_trylock(&acc->mutex) != 0){
+				printf("Error attempting to lock already locked mutex: %d\n", pthread_mutex_trylock(&acc->mutex));
+				write(sock, request, sprintf(request, "Bank: Waiting to start customer session for account %s.\n", token) + 1);
+				sleep(2);
+			}
+			
+			client->acc = acc;
+			client->acc->active = true;
+
+			write(sock, request, sprintf(request, "Bank: Successfully connected to account %s.\n", token) + 1);
+		} 
+		else if (streq(token, "deposit")) {
+			if (!client->acc || !client->acc->active) {
+				write(sock, request, sprintf(request, "Bank: Not currently in a customer session.\n") + 1);
+			}
+			else if (!(token = getNextToken(tokenizer)) || getNextToken(tokenizer)) 			{
+				write(sock, request, sprintf(request, "Bank: Invalid syntax.\n") + 1);
+			} 
+			else if (!(change = atof(token)) || change <= 0) {
+				write(sock, request, sprintf(request, "Bank: Not a valid deposit.\n") + 1);
+			}
+			else {
+				write(sock, request, sprintf(request, "Old Balance:\t$%.2lf\nDepositing:\t$%.2lf\nNew Balance:\t$%.2lf\n", client->acc->balance, change, (client->acc->balance + change)) + 1);
+				client->acc->balance += change;
+			}
+		} 
+		else if (streq(token, "withdraw")) {
+			if (!client->acc || !client->acc->active) {
+				write(sock, request, sprintf(request, "Bank: Not currently in a customer session.\n") + 1);
+			}
+			else if (!(token = getNextToken(tokenizer)) || getNextToken(tokenizer)) {
+				write(sock, request, sprintf(request, "Bank: Invalid syntax.\n") + 1);
+			}
+			else if (!(change = atof(token)) || change <= 0) {
+				write(sock, request, sprintf(request, "Bank: Not a valid withdrawal.\n") + 1);
+			}
+			else if (change > client->acc->balance) {
+				write(sock, request, sprintf(request, "Bank: Not enough funds to withdraw.\n") + 1);
+			}
+			else {
+				write(sock, request, sprintf(request, "Old Balance:\t$%.2lf\nDepositing:\t$%.2lf\nNew Balance:\t$%.2lf\n", client->acc->balance, change, (client->acc->balance - change)) + 1);
+				client->acc->balance -= change;
+			}
+		} 
+		else if (streq(token, "query")) {
+			if (!client->acc || !client->acc->active) {
+				write(sock, request, sprintf(request, "Bank: Not currently in a customer session.\n") + 1);
+			}
+			else if (getNextToken(tokenizer)) {
+				write(sock, request, sprintf(request, "Bank: Invalid syntax.\n") + 1);
+			}
+			else {
+				write(sock, request, sprintf(request, "Balance: $%.2lf\n", client->acc->balance) + 1);
+			}
+		} 
+		else if (streq(token, "end")) {
+			if (!client->acc || !client->acc->active) {
+				write(sock, request, sprintf(request, "Bank: Not currently in a customer session.\n") + 1);
+			} else {
+				write(sock, request, sprintf(request, "Bank: Ending customer session to account %s.\n", client->acc->name) + 1);
+				client->acc->active = false;
+				pthread_mutex_unlock(&client->acc->mutex);
+				client->acc = NULL;
+			
+				printf("Ended customer session and unlocked account mutex\n");
+			}
+		} 
+		else if (streq(token, "quit")) {
+			write(sock, request, sprintf(request, "Bank: Disconnecting from Server and ending Client thread.\n") + 1);
+
+			if (client->acc){
+				client->acc->active = false;
+				pthread_mutex_unlock(&client->acc->mutex);
+			}
+
+			pthread_mutex_lock(&clients_mutex);
+			removeLinkedNode(client, clients);
+			pthread_mutex_unlock(&clients_mutex);
+			closeClient(client);
+			close(sock);
+
+			destroyTokenizer(tokenizer);
+			//Break out of this while loop and return NULL effectively ending the thread.
+			break;
+		} 
+		else {
+			write(sock, request, sprintf(request, "Invalid syntax. Proper commands:\ncreate <account_name> \tcreates an account\nserve <account_name>\tstarts a customer session\ndeposit <amount>\tdeposits amount into account\nwithdraw <amount>\twithdraws amount from account\nquery\t\t\tprints account balance\nend\t\t\tends customer session\nquit\t\t\tends client session\n") + 1);
+		}
+
+		destroyTokenizer(tokenizer);
+	}
     }
 
     close(sock);
