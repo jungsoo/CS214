2266f791b64a89208783c414f7e728cae96c64ec
Jungsoo Park <jungsoopark96@gmail.com>
2015-12-06 22:36:12 -0500

    Implement timer... needs work.

--- server.c
+++ server.c
@@ -190,8 +190,29 @@ void client_service(int sock) {
     close(sock);
 }
 
+void print_account_info(int acc_index) {
+    printf("\tAccount Name: %s\n", bank[acc_index].name);
+    printf("\tBalance: $%.2lf\n", bank[acc_index].balance);
+    if (bank[acc_index].is_active) {
+        printf("\n\tIN SESSION\n");
+    }
+    printf("\n");
+}
+
 void print_bank_info(int signum) {
-    // TODO
+    pthread_mutex_lock(&accounts_mutex);
+    printf("--- BANK OF JUNGSOO ---\n\n");
+    for (int i = 0; i < MAX_ACCOUNT; i++) {
+        if (!streq(bank[i].name, "")) {
+            print_account_info(i);
+            continue;
+        }
+        if (i == 0) {
+            printf("...Bank is empty. :(\n\n");
+        }
+    }
+    printf("--------- END ---------\n\n");
+    pthread_mutex_unlock(&accounts_mutex);
 }
 
 void set_timer(int seconds) {
@@ -226,7 +247,6 @@ int main() {
     serv_addr.sin_port = htons(__SERVER_PORT__);
 
     // Display server information every 20 seconds.
-    /*
     struct sigaction timer;
     sigemptyset(&timer.sa_mask);
     timer.sa_flags = 0;
@@ -234,7 +254,6 @@ int main() {
     sigaction(SIGALRM, &timer, NULL);
 
     set_timer(20);
-    */
 
     // Server exit handler
     /* Do we really need sigaction?
